57e3293c8f648f14a32b81c30da52ea1a1a7a407
Sam Piggott <sam@raw-pixel.com>
2015-09-14 01:46:41 +0100

    Added fix to library

--- SorryWereOpen.swift
+++ SorryWereOpen.swift
@@ -19,8 +19,10 @@ class SorryWereOpen {
             return components.date
         }
         
-        if let date1970 = groundDate(date), let firstDate1970 = groundDate(firstDate), var lastDate1970 = groundDate(lastDate) {
-            if firstDate1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { lastDate1970 = lastDate1970.dateByAddingTimeInterval(86400) }
+        if var date1970 = groundDate(date), let firstDate1970 = groundDate(firstDate), var lastDate1970 = groundDate(lastDate) {
+            if firstDate1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { lastDate1970 = lastDate1970.dateByAddingTimeInterval(86400)
+                if date1970.compare(firstDate1970) == NSComparisonResult.OrderedAscending { date1970 = date1970.dateByAddingTimeInterval(86400) }
+            }
             if date1970.compare(firstDate1970) == NSComparisonResult.OrderedAscending { return false }
             if date1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { return false }
             return true
