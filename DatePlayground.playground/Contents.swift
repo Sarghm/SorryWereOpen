399bfab4c52081a731d771c75c539e6ff99e5358
Sam Piggott <sam@raw-pixel.com>
2015-09-14 01:45:42 +0100

    Bug fix
    If the user was checking an early morning event in the early morning,
    it would report as closed (as it failed the first test).

--- DatePlayground.playground/Contents.swift
+++ DatePlayground.playground/Contents.swift
@@ -13,8 +13,10 @@ class func checkIfTime(date:NSDate!, isBetween firstDate:NSDate!, andTime lastDa
         return components.date
     }
     
-    if let date1970 = groundDate(date), let firstDate1970 = groundDate(firstDate), var lastDate1970 = groundDate(lastDate) {
-        if firstDate1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { lastDate1970 = lastDate1970.dateByAddingTimeInterval(86400) }
+    if var date1970 = groundDate(date), let firstDate1970 = groundDate(firstDate), var lastDate1970 = groundDate(lastDate) {
+        if firstDate1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { lastDate1970 = lastDate1970.dateByAddingTimeInterval(86400)
+            if date1970.compare(firstDate1970) == NSComparisonResult.OrderedAscending { date1970 = date1970.dateByAddingTimeInterval(86400) }
+        }
         if date1970.compare(firstDate1970) == NSComparisonResult.OrderedAscending { return false }
         if date1970.compare(lastDate1970) == NSComparisonResult.OrderedDescending { return false }
         return true
@@ -40,4 +42,4 @@ let sixAtNight = NSDateComponents(); sixAtNight.calendar = calendar
 sixAtNight.hour = 18; sixAtNight.minute = 0; sixAtNight.second = 0;
 
 // And here's the li'l fella in action.
-TimeChecker.checkIfTime(todaysDate, isBetween: elevenInTheMorning.date, andTime: threeInTheMorning.date)
+TimeChecker.checkIfTime(todaysDate, isBetween: sixAtNight.date, andTime: threeInTheMorning.date)
